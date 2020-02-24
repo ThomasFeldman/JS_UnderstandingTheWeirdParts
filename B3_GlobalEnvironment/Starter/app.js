b3ed646feee786e2904592cd59c790db68ae63b7
ThomasFeldman <tfeldman1.31@gmail.com>
2020-02-24 06:22:32 -0600

    Functions, Context, and Variable Environments

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -1,8 +1,15 @@
-b();
-console.log(a);
-
-var a = 'Hello World!';
-
 function b() {
-    console.log('Called b!');
+    var myVar;
+    console.log(myVar);
 }
+
+function a() {
+    var myVar = 2;
+    console.log(myVar);
+    b();
+}
+
+var myVar = 1;
+console.log(myVar);
+a();
+console.log(myVar);
