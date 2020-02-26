da6bc723fa6a476bc5873dc177ab9f9a087d184b
ThomasFeldman <tfeldman1.31@gmail.com>
2020-02-26 12:26:19 -0600

    Objects and Object Literals

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -1,21 +1,26 @@
-var person = new Object();
+var Tony = { 
+    firstname: 'Tony',
+    lastname: 'Alicea',
+    address: {
+        street: '111 Main St.',
+        city: 'New York',
+        state: 'NY'
+    }
+};
 
-person["firstname"] = "Tony";
-person["lastname"] = "Alicea";
+function greet (person) {
+    console.log('Hi ' + person.firstname);
+}
 
-var firstNameProperty = "firstname";
+greet(Tony);
 
-console.log(person);
-console.log(person[firstNameProperty]);
+greet({
+    firstname: 'Mary',
+    lastname: 'Doe'
+});
 
-console.log(person.firstname);
-console.log(person.lastname);
+Tony.address2 = {
+    street: '333 Second St.'
+}
 
-person.address = new Object();
-person.address.street = "111 Main St.";
-person.address.city = "New York";
-person.address.state = "NY";
-
-console.log(person.address.street);
-console.log(person.address.city);
-console.log(person["address"]["state"]);
+console.log(Tony);
